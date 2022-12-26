d55f701eb4eb634ad54d9d9c716193026e4941a4
rod08018 <rod08018@gmail.com>
2022-12-26 09:45:06 -0600

    fixing heroku db connection

--- database.py
+++ database.py
@@ -4,10 +4,12 @@ from sqlalchemy import create_engine
 import os
 import psycopg2
 load_dotenv()
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
 db_uri = os.getenv("db_uri")
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+sqlEngine = create_engine(DATABASE_URL)
+
+conn = sqlEngine.connect()
 
 
 def manage_db_watch_data(watch_data: dict):
